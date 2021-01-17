8d63e9ece0618fec21e93626012b55479ae22670
Sam Huynh <samhwang2112.dev@gmail.com>
2021-01-17 23:25:16 +1100

    Move all buttons into an array

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -4,27 +4,26 @@ import { useStyles } from '../hooks';
 
 function App() {
   const classes = useStyles();
+  const grids = [
+    { label: 'Work Length', initialValue: 25 },
+    { label: 'Short Break Length', initialValue: 5 },
+    { label: 'Long Break Length', initialValue: 15 },
+    { label: 'Interval', initialValue: 4 },
+  ];
 
   return (
     <>
       <CssBaseline />
       <Grid container spacing={1} className={classes.root}>
-        <Typography variant="h1" paragraph>
+        <Typography variant="h2" paragraph>
           Pomodoro Timer
         </Typography>
         <Grid container item xs={12} spacing={2}>
-          <Grid item xs={3}>
-            <ButtonWithCounter label="Work Length" initialValue={25} />
-          </Grid>
-          <Grid item xs={3}>
-            <ButtonWithCounter label="Short Break Length" initialValue={5} />
-          </Grid>
-          <Grid item xs={3}>
-            <ButtonWithCounter label="Long Break Length" initialValue={15} />
-          </Grid>
-          <Grid item xs={3}>
-            <ButtonWithCounter label="Interval" initialValue={4} />
-          </Grid>
+          {grids.map(({ label, initialValue }) => (
+            <Grid item xs={3} key={label}>
+              <ButtonWithCounter label={label} initialValue={initialValue} />
+            </Grid>
+          ))}
         </Grid>
       </Grid>
     </>
