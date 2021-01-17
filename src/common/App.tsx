9d12140c58e83563cff4c27c001fc51d6111c064
Sam Huynh <samhwang2112.dev@gmail.com>
2021-01-17 23:05:26 +1100

    Change the main layout

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,25 +1,30 @@
+import { CssBaseline, Grid, Typography } from '@material-ui/core';
+import TimerLengthButton from '../components/TimerLengthButton';
 import { useStyles } from '../hooks';
 
 function App() {
   const classes = useStyles();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Grid container spacing={1} className={classes.root}>
+        <Typography variant="h1" paragraph>
+          Pomodoro Timer
+        </Typography>
+        <Grid container item xs={12} spacing={2}>
+          <Grid item xs={4}>
+            <TimerLengthButton type="Work" initialValue={25} />
+          </Grid>
+          <Grid item xs={4}>
+            <TimerLengthButton type="Short Break" initialValue={5} />
+          </Grid>
+          <Grid item xs={4}>
+            <TimerLengthButton type="Long Break" initialValue={15} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
