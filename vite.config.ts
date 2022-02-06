1f0e385c3853714c5e462ddc27bcb0fc92b01d93
Sam Huynh <samhwang2112.dev@gmail.com>
2022-02-06 11:17:48 +1100

    Move app base to /pomodoro-timer

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/pomodoro-timer/',
   plugins: [
     react(),
     visualizer({
